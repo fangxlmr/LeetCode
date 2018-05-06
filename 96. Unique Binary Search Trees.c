4bc0a5e2b7e02df2dca5004fb7c2fbddddd354a2
fangxlmr <fangxlmr@foxmail.com>
2018-05-06 20:20:10 +0800

    add file: 96

---
+++ 96. Unique Binary Search Trees.c
@@ -0,0 +1,32 @@
+/* The recursive solution works, but Time Limit Exceeded on LeetCode */
+/*
+int numTrees(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+
+    int num = 0, i;
+    for (i = 0; i < n; ++i) {
+        num += numTrees(i) * numTrees(n - 1 - i);
+    }
+    return num;
+}
+*/
+
+/* iteration solution */
+int numTrees(int n) {
+    if (n < 0) {
+        return 0;
+    }
+
+    int num[n + 1];
+    memset (num, 0, sizeof(num));
+    int i, j;
+    num[0] = num[1] = 1;
+    for (i = 2; i <= n; ++i) {
+        for (j = 0; j < i; ++j){
+            num[i] += num[j] * num[i - 1 - j];            
+        }
+    }
+    return num[n];
+}
