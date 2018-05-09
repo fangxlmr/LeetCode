dbd480e6a6e84c8849404aa8634db0bf4b890f8b
fangxlmr <fangxlmr@foxmail.com>
2018-05-09 22:11:02 +0800

    fix probs in 51 and 95

--- 51. N-Queens.c
+++ 51. N-Queens.c
@@ -1,28 +1,24 @@
 /**
  * Return an array of arrays of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
- */
 
-/* 伪代码 */
-/*
-  MAX = 8   // 棋盘大小
-  int queen[MAX]    // 一维结果数组，每个皇后的坐标为(i, queen[i]), i = 1, 2, ... MAX
 
-void nqueen (int n) {
-    for (i = 1; i <= MAX; ++i){
+
+ * 伪代码
+ * 核心函数
+    void nqueen (int n) {
+        for (i = 1; i <= MAX; ++i){
           queen[n] = i; //将横坐标为n的皇后，从1到MAX列，循环一遍
           if (check(n)) {   //判断是否位置是否合法
             if (n == MAX)
                 printf the result;
             else
-                nqueen(n + 1);  // 继续摆放下一个皇后 
+                nqueen(n + 1);  // 继续摆放下一个皇后
           }
+        }
     }
 
-}
-
-
-*/
+ **/
 #define DEBUG 1
 #if DEBUG
 #include <stdio.h>
@@ -46,9 +42,10 @@ int *result[MAX];  /* 预分配MAX个字节的结果 */
 
 #if DEBUG
 int main (){
-    int *returnSize;
-    *returnSize = 0;
-    int n = 5;
+    int tmp, *returnSize = &tmp;
+    *returnSize = 3;
+    int n = 4;
+    printf("here\n");
     char ***result = solveNQueens (n, returnSize);
     int i, j;
     for (i = 0; i < *returnSize; ++i) {
@@ -62,67 +59,78 @@ int main (){
 #endif
 
 char*** solveNQueens(int n, int* returnSize) {
-    int *queen = (int *) malloc (n * sizeof(int));   /* 每个皇后的坐标为 (n, queen[n]) */
+    int *queen = (int *) malloc (n * sizeof(int));
+/* 每个皇后的坐标为 (n, queen[n])*/
+
     memset (queen, 0, sizeof(queen));
     N = n;
-
     nqueen (0, queen);
     *returnSize = count;
-    return output (n);;
+    return output (n);
 }
 
 
-/* 回朔法的核心函数 */
+
+/* 回朔法的核心函数*/
+
 void nqueen (int row, int *queen) {
     int i;
-    if (row == N){    /* 循环完毕，保存结果 */
+    if (row == N){
+/* 循环完毕，保存结果*/
+
         int *ans = (int *) malloc (N * sizeof(int));
         memcpy(ans, queen, N * sizeof(int));
         save(ans);
         return;
     }
+    /* 将横坐标为row的皇后，从第一列到第N列，全部测试一遍*/
     for (i = 0; i < N; ++i) {
-        *(queen + row) = i;   /* 将横坐标为row的皇后，从第一列到第N列，全部测试一遍 */
-        if (check(row, queen))    /* 判断当前位置时候合法 */
-            nqueen(row + 1, queen);      /* 填写下一行皇后位置 */
+        *(queen + row) = i;
+        if (check(row, queen))  /* 判断当前位置时候合法*/
+            nqueen(row + 1, queen);     /* 填写下一行皇后位置*/
+
     }
 }
 
 
-/* 将合法结果保存到result中 */
+/* 将合法结果保存到result中*/
 void save (int *ans) {
     result[count]= ans;
-    ++count;    /* 下一个空闲位置 */
+    ++count; /* 下一个空闲位置*/
     return;
 }
 
 
-/* 检查(n, queen[n]) 皇后的合法性 */
+/* 检查(n, queen[n]) 皇后的合法性*/
 bool check (int row, int *queen) {
     int i;
-    for (i = 0; i < row; ++i) {      /* 判断当前n皇后与之前的皇后是否  同列，同对角线 */
+    for (i = 0; i < row; ++i) {     /*判断当前n皇后与之前的皇后是否  同列，同对角线*/
         if (queen[row] == queen[i] || abs(queen[row] - queen[i]) == (row - i))
-            return false;   /* 若满足不合法条件，则返回不合法位置 */
+            return false;   /* 不合法*/
+
     }
     return true;
 }
 
 
-/* 打印出合法结果, n列  */
+/* 打印出合法结果, n列*/
 char ***output (int n) {
-    /* 有效解集合对应的数组为 matrix */
+/* 有效解集合对应的数组为 matrix*/
     char ***matrix = NULL;
     if (count == 0) {
+        matrix = (char ***) calloc (1, sizeof(char **));
+        matrix[0] = (char **) calloc(1, sizeof(char *));
         return matrix;
-        matrix =  (char ***) calloc (1, sizeof(char **));
     }
     else {
         matrix =  (char ***) calloc (count, sizeof(char **));
         int i, j;
-        for (i = 0; i < count; ++i) {     /* 遍历 result数组 */
-            /* 每个有效解对应的矩阵为each_matrix */
+        for (i = 0; i < count; ++i) {   /* 遍历 result数组*/
+
+            /* 每个有效解对应的矩阵为each_matrix*/
             char **each_matrix = (char **) calloc (n, sizeof(char *));
-            for (j = 0; j < n; ++j) {   /* 输出每行的值 */
+            for (j = 0; j < n; ++j) {
+            /*输出每行的值*/
                 *(each_matrix + j) = line (n, *(result[i] + j));
             }
             *(matrix + i) = each_matrix;
@@ -132,9 +140,8 @@ char ***output (int n) {
 }
 
 
-/* n个位的字符串，第i位为Q，其余为dot "." */
+/* n个位的字符串，第i位为Q，其余为dot "."*/
 char *line (int n, int i) {
-    /* n 位字符串，需要n + 1个字节 */
     char *str = (char *) calloc ((n + 1), sizeof(char));
     int j;
     for (j = 0; j < n; ++j)
--- 95. Unique Binary Search Trees II.c
+++ 95. Unique Binary Search Trees II.c
@@ -10,9 +10,10 @@
  * Return an array of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
  */
+#include <stdio.h>
 
 typedef struct TreeNode TreeNode, *pTreeNode;
 
 struct TreeNode** generateTrees(int n, int* returnSize) {
-    
+
 }
