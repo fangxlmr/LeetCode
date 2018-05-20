ea0e9b8cc48a110d5f4c0b14cbe2728eaaf22dc9
fangxlmr <fangxlmr@foxmail.com>
2018-05-21 03:12:14 +0800

    solve the prob 8.

--- 8. String to Integer (atoi).c
+++ 8. String to Integer (atoi).c
@@ -1,59 +1,21 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <limits.h>
-#include <stdbool.h>
-#include <string.h>
-
-int myAtoi(char *);
-
-int main(){
-    char *s = "9223372036854775809";
-    printf("s = %s, len = %d\n", s, strlen(s));
-    printf("myAtoi(s) = %d\n", myAtoi(s));
-    return 0;
-}
-
-
 int myAtoi(char* str) {
-    int len = strlen(str);
-    if(!len){ /* 空字符串 */
-        return 0;
-    }
-
+    int base = 0, sign = 1;  /* base 为返回的数值， sign为符号 */
     int i = 0;
-    long long int result = 0;
-    int sign = 1;   /*符号位，正数为1，负数为-1*/
-    while(i < len){
-        if(isspace(str[i])){  /* 判断开头空格 */
-            ++i;
-            continue;
-        }
-        else{
-            break;
-        }
+    while (isspace(str[i])) {   /* 去掉开头的空格 */
+        ++i;
     }
-    while(i < len){
-        if(str[i] == 43 || str[i] == 45){   /* 判断正负号 */
-            sign = 44 - str[i];   /* 若是+号，assic码为43，-号为45，result = 44 - str[i]刚好是符号位 */
-            ++i;
-        }
-        if(!isdigit(str[i])){
-            break;
-        }
-        else{    /* 数字字符 */
-            result *= 10;
-            result += str[i] - 48;
-            ++i;
-        }
+    if (str[i] == '-' || str[i] == '+') {
+        sign = (str[i] == '-') ? -1 : 1;
+        ++i;
     }
-
-    if(result == 0)
-        return result;
-    if(sign > 0){
-        return result > INT_MAX ? INT_MAX : result; 
+    while (isdigit(str[i])) {
+        if (base > INT_MAX / 10 ||      /* 判断是否溢出 */
+            INT_MAX - base * 10 < (str[i] - '0'))
+        {
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+        base = base * 10 + (str[i] - '0');
+        ++i;
     }
-    else{
-        result = sign * result;
-        return result < INT_MIN ? INT_MIN : result;
-    }
+    return sign * base;
 }
