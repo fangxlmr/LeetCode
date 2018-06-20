f846a7568ff7d94f144a966eef090889b1b01580
fangxlmr <fangxlmr@foxmail.com>
2018-06-20 18:00:31 +0800

    update 15

--- 15. 3Sum.c
+++ 15. 3Sum.c
@@ -2,25 +2,27 @@
  * Return an array of arrays of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
  */
-void quick_sort (int *nums, int left, int right);
-int** threeSum(int* nums, int numsSize, int* returnSize) {
-    *returnSize = 0;
-    if (numsSize == 0) {
-        return NULL;
-    }
 
-    /* 定义变量 */
+//void quick_sort (int *nums, int left, int right);
+int cmp(const void *a, const void *b)
+{
+    return (int *)a - (int *)b;
+}
+int** threeSum(int* nums, int numsSize, int* returnSize) {
     int **ret, *each;    /* ret 为二维数组，each为一维数组 */
     unsigned int size;       /* ret数组的长度 */
     int fix, left, right;    /* fix为第一个固定数， left right 为双指针 */
     int sum, target;    /* 临时和变量 */
 
-    /* 变量初始化 */
+    if (numsSize == 0) {
+        return NULL;
+    }
+
     size = pow(numsSize, 3) / 6;    /* 最多有 numsSize^3 / 6个可能 */
-    printf("size = %d\n", size);
     ret = (int **) malloc(size * sizeof(int *));
     *returnSize = 0;
 
+    qsort(nums, numsSize, sizeof(*nums), cmp);
     quick_sort(nums, 0, numsSize - 1);   /* 排序 */
     for (fix = 0; fix <= numsSize - 3; ++fix) {    /* fix第一位数 */
         if (nums[fix] > 0) {   /* 剪枝优化，第一个数大于0，后面不可能有有效解 */
@@ -31,7 +33,7 @@ int** threeSum(int* nums, int numsSize, int* returnSize) {
         if (fix != 0 && nums[fix] == nums[fix - 1]) {
             continue;
         }
-        target = 0 - nums[fix];       /* 另外两个数的为 0 - fix */
+        target = 0 - nums[fix];       /* 另外两个数的和为 0 - fix */
         left = fix + 1;       /* 双指针设定值 */
         right = numsSize - 1;
         while (left < right) {  /* 判断双指针对应的和是否为target */
@@ -50,10 +52,7 @@ int** threeSum(int* nums, int numsSize, int* returnSize) {
                 each[0] = nums[fix];
                 each[1] = nums[left];
                 each[2] = nums[right];
-                /* 保存有效解 */
-                ret[*returnSize] = each;
-                (*returnSize)++;
-                /* 继续下移 */
+                ret[(*returnSize)++] = each;
                 ++left;
             } else if (sum < target) {     /* 偏小，left右移 */
                 ++left;
@@ -65,34 +64,32 @@ int** threeSum(int* nums, int numsSize, int* returnSize) {
     return ret;
 }
 
+/*
+static void swap(int *v, int x, int y)
+{
+    int temp;
 
-/* 快速排序 */
-void quick_sort (int *nums, int left, int right) {
-    if (left > right) {
+    temp = v[x];
+    v[x] = v[y];
+    v[y] = temp;
+}
+
+
+void quick_sort (int *v, int left, int right) {
+    int i, last;
+
+    if(left >= right) {
         return;
     }
-
-    int i, j, pivot, tmp;
-
-    i = left;
-    j = right;
-    pivot = nums[left];
-    while (i < j) {
-        while (nums[j] >= pivot && i < j) {
-            --j;
-        }
-        while (nums[i] <= pivot && i < j) {
-            ++i;
-        }
-        if (i < j) {
-            tmp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = tmp;
+    swap(v, left, (left + right)/2);
+    last = left;
+    for (i = left + 1; i <= right; ++i) {
+        if (v[i] < v[left]) {
+            swap(v, ++last, i);
         }
     }
-    nums[left] = nums[i];
-    nums[i] = pivot;
-
-    quick_sort(nums, left,  i - 1);
-    quick_sort(nums, i + 1, right);
+    swap(v, left, last);
+    quick_sort(v, left, last - 1);
+    quick_sort(v, last + 1, right);
 }
+ */
