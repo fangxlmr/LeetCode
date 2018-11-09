70b1bb7e392298913f1a1186d9c336015ccc202b
fangxl <fangxlmr@foxmail.com>
2018-11-09 09:06:10 +0800

    add 81 and cleanup 33

--- 81. Search in Rotated Sorted Array II.c
+++ 81. Search in Rotated Sorted Array II.c
@@ -1,4 +1,4 @@
-int search(int* nums, int numsSize, int target) {
+bool search(int* nums, int numsSize, int target) {
     int low, mid, high;
 
     low = 0;
@@ -6,20 +6,22 @@ int search(int* nums, int numsSize, int target) {
     while (low <= high) {
         mid = low + ((high - low) >> 1);
         if (nums[mid] == target) {
-            return mid;
-        } else if (nums[mid] > nums[high]) {
+            return true;
+        } else if (nums[mid] > nums[high]){
             if (nums[low] <= target && target < nums[mid]) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
-        } else {
+        } else if(nums[mid] < nums[high]) {
             if (nums[mid] < target && target <= nums[high]) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
+        } else {
+            high--;
         }
     }
-    return -1;
+    return false;
 }
