2e9b3ea15b8eb69ee2118961b3d9541a58a1f8fe
fangxlmr <fangxlmr@foxmail.com>
2018-12-13 23:34:36 +0800

    modify 84

--- 84. Largest Rectangle in Histogram.c
+++ 84. Largest Rectangle in Histogram.c
@@ -16,18 +16,20 @@ int largestRectangleArea(int* heights, int heightsSize) {
     if (heightsSize <= 0) {
         return 0;
     }
-    res = dp[0][0] = dp[0][1] = heights[0];
+    res = dp[0][0] = heights[0];
+    dp[0][1] = 1;
     for (i = 1; i < heightsSize; ++i) {
         x = i % 2;
         y = (i - 1) % 2;
 
-        min = twoMin(dp[y][1], heights[i]);
-        count = dp[y][1] == 0 ? 0 : dp[y][0] / dp[y][1];
+        count = dp[y][1];
+        min = twoMin(dp[y][0] / count, heights[i]);
         if ((count + 1) * min > heights[i]) {
             dp[x][0] = (count + 1) * min;
-            dp[x][1] = min;
+            dp[x][1] = count + 1;
         } else {
-            dp[x][0] = dp[x][1] = heights[i];
+            dp[x][0] = heights[i];
+            dp[x][1] = 1;
         }
         res = twoMax(dp[x][0], res);
     }
