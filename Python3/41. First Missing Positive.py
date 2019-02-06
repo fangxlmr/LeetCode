e8c8b7eb7c817ed05280194b054f82590efb4d11
fangxlmr <fangxlmr@foxmail.com>
2019-02-06 20:15:17 +0800

    append 41.py

--- Python3/41. First Missing Positive.py
+++ Python3/41. First Missing Positive.py
@@ -11,3 +11,16 @@ class Solution:
             if index + 1 != item:
                 return index + 1
         return item + 1
+
+
+class Solution:
+    def firstMissingPositive(self, nums: 'List[int]') -> 'int':
+        n = len(nums)
+        for i in range(n):
+            while 0 < nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+
+        for i, _ in enumerate(nums):
+            if nums[i] - 1 != i:
+                return i + 1
+        return n + 1
