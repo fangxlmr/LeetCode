0e860b1769994edd76d29f07ddc762458bd001da
fangxlmr <fangxlmr@foxmail.com>
2019-03-21 21:02:45 +0800

    Update 893.py.

--- Python3/893. Groups of Special-Equivalent Strings.py
+++ Python3/893. Groups of Special-Equivalent Strings.py
@@ -4,10 +4,13 @@ class Solution:
         I understand the problem, but no proper solution afterward.
         This solution copied from LeetCode.com is so genius.
         """
-        def count(A):
-            ans = [0] * 52
-            for i, letter in enumerate(A):
-                ans[ord(letter) - ord('a') + 26 * (i%2)] += 1
-            return tuple(ans)
+#         def count(A):
+#             ans = [0] * 52
+#             for i, letter in enumerate(A):
+#                 ans[ord(letter) - ord('a') + 26 * (i%2)] += 1
+#             print(tuple(ans))
+#             return tuple(ans)
 
-        return len({count(word) for word in A})
+#         return len({count(word) for word in A})
+
+        return len({"".join(sorted(s[0::2])) + "".join(sorted(s[1::2])) for s in A})
